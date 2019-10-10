3c1e4436b13cf6daf4fd0a5fc894fcbde5e2c7de
ThomasTrivier <ratchethomas@outlook.com>
2019-10-10 10:15:57 +0200

    Table Users définie

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('role')->default('user');
+            $table->string('role')->default('guest');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('newsletter')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
