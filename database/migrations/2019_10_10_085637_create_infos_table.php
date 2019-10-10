e8cab59967ad4f5671512f1c6c7ef41210f749c9
ThomasTrivier <ratchethomas@outlook.com>
2019-10-10 11:02:33 +0200

    Création Table Infos et définie

---
+++ database/migrations/2019_10_10_085637_create_infos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('infos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('building_name');
+            $table->string('adress_part_1');
+            $table->string('adress_part_2');
+            $table->string('title');
+            $table->string('tel');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('infos');
+    }
+}
