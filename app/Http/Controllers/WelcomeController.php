87cb55fdf16dc15d1b5700146e350e7477ff9c6d
ThomasTrivier <ratchethomas@outlook.com>
2019-10-11 15:40:44 +0200

    Section Services finie

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Navbar;
 use App\About;
+use App\SectionService;
+use App\Service;
 use App\Info;
 
 class WelcomeController extends Controller
@@ -12,7 +14,9 @@ class WelcomeController extends Controller
     public function index(){
         $nav = Navbar::find(1);
         $about = About::find(1);
+        $sectionS = SectionService::find(1);
+        $services = Service::all();
         $infos = Info::find(1);
-        return view('welcome', compact('nav','about','infos'));
+        return view('welcome', compact('nav','about','sectionS','services','infos'));
     }
 }
